febd9436ba546d692a8e4462fda5a62575882089
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-01-10 15:59:28 -0800

    got form to render

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Authorize from './services/views/Authorize';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/">
-            homepage
-          </Route>
+          <Route exact path="/" component={Authorize}></Route>
         </Switch>
       </BrowserRouter>
     </div>
--- src/services/views/Authorize.js
+++ src/services/views/Authorize.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function Authorize() {
+  const [loading, setLoading] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
       <form>
         <label>
-          <input type="text" placeholder="email">
-            email
-          </input>
+          <input type="text" placeholder="email"></input>
         </label>
         <label>
-          <input type="password" placeholder="password">
-            password
-          </input>
+          <input type="password" placeholder="password"></input>
         </label>
         <button>login</button>
       </form>
