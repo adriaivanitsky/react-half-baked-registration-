cf04cf098dd78ea865c9d79ed867c956df736311
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-01-10 21:08:13 -0800

    added logout button to redirect page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Authorize from './views/Authorize';
 import { getUser } from './services/users';
 import { useState } from 'react';
+import { logout } from './services/users';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(getUser());
+  const logoutUser = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
 
   return (
     <div className="App">
@@ -13,7 +18,10 @@ function App() {
         <Switch>
           <Route exact path="/">
             {currentUser ? (
-              <h1>you did it!</h1>
+              <div>
+                <button onClick={(e) => logoutUser(e.target.value)}>logout</button>
+                <h1>you did it!</h1>
+              </div>
             ) : (
               <Authorize currentUser={currentUser} setCurrentUser={setCurrentUser} />
             )}
